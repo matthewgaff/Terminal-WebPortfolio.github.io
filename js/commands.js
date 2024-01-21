2568f072a6aeaee49424329967aac6fc414fe09c
Matthew Gaffoor <96254954+matthewgaff@users.noreply.github.com>
2024-01-21 00:43:18 -0400

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,115 @@
+var password = "Booboos";
+var linkedin = "https://www.linkedin.com/in/matthew-gaffoor-b6040924a/";
+var instagram = "https://www.instagram.com/matchchew_/";
+var github = "https://github.com/matthewgaff/";
+var email = 'mailto:matthewgaffoor9@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Matthew!👋",
+  "I'm a developer, who builds engaging websites like this one",
+  "and loves to talk about computer science, software engineering, food, cars, sports, basically a know it all ;).",
+  "After graduating SBCS with 1 yr Diploma in Computing,",
+  "a 2 yr Diploma in Information Technology Network Speacialist with NESC, ",
+  "I found as a aspiring developer, it is important to build my skill, ", 
+  "build enterprise web applications for some of the world's BIGGEST companies.",
+  "While doing all of that, I learned alot of languages such as - Python, Java, Javascript, HTML, CSS, ",
+  "but my favourite was Web Development.",
+  "I fell in love with the concept of making websites and the functions and features you can apply. ",
+  "So I created a roadmap to becoming a Web Developer, began coding and mastered the art of Web Develoment. ",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "I'm you from a different universe,",
+  "but I'm way more handsome, funnier, smarter, taller, stronger and just better in every way...",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/matthew gaffoor' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/matchchew_' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/matthewgaff' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin or a HACKER.',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "My projects are on Github.",
+  "To see them, type social & click Github ;) ",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Matthew A. Gaffoor?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Matthew A. Gaffoor (MAG) Not A Corporation. All Gaffoors reserved.</span>',                                                                                                                                                                                                                                                                                                            
+                                                                                                         
+                                                                                                            
+"                                                                                                            ",                                                                                                          
+"                                                 ....                                                       ",
+"                                                ........                                                    ",
+"                                              +...........                                                  ",
+"                                             ................                                               ",
+"                                            ...................                                             ",
+"                                          -.......................                                          ",
+"                                         ...........................                                        ",
+"                                        ...............................                                     ",
+"                                       ..................................                                   ",
+"                                        ....................................                                ",
+"                                          ............-.......................-                             ",
+"                                           .............#........................                           ",
+"                                             ............-#.........................                        ",
+"                                              -............##-........................                      ",
+"                                                ............-  #.......................#+                   ",
+"                             -....                ............#  #....................#####                 ",
+"                            ..........-            ............-   ##...............-######                 ",
+"                           .................         ............#    #............#######                  ",
+"                         -......................-     ............-     ##........######                    ",
+"                        .............................   ............#      #-....######                     ",
+"                       ..............................................+       #########                      ",
+"                      .................................................#        ####                        ",
+"                    ....................................................+         #                         ",
+"                   ....................#..................................#                                 ",
+"                    ......................###.............................##                                ",
+"                      -......................#####.......................#####                              ",
+"                       ##.......................+######................-######                              ",
+"                          #+.......................  #######..........#######                               ",
+"                            ##........................    #######....######                                 ",
+"                               ##.......................       ###########                                  ",
+"                                  #........................         #####                                   ",
+"                                    ##.......................                                               ",
+"                                       #-.......................                                            ",
+"                                         ##...................###                                           ",
+"                                            #+...............######                                         ",
+"                                              ##............######                                          ",
+"                                                 ##.......+######                                           ",
+"                                                    #....#######                                            ",
+"                                                      ########                                              ",
+"                                                         ####                                               ",
+"                                                                                             © 2024         ",                                                                                                            
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
